35036cbe2c0ff9e88ecee7cc6d108473fed010f8
Ayelet Dahan <anativ@gmail.com>
2019-07-17 17:09:53 +0300

    basic testing comic component

--- src/app/comic/comic.component.spec.ts
+++ src/app/comic/comic.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Router, ActivatedRoute } from '@angular/router';
 import { convertToParamMap } from '@angular/router';
@@ -6,27 +6,35 @@ import { convertToParamMap } from '@angular/router';
 
 import { ComicComponent } from './comic.component';
 import { XkcdService } from '../xkcd.service';
-import { Observable } from 'rxjs';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
+
 
 describe('ComicComponent', () => {
-  let xkcdServiceStub: Partial<XkcdService>;
   let router: Router;
-  let xkcdService: Partial<XkcdService>; 
+  let xkcdService: Partial<XkcdService>;
   let component: ComicComponent;
   let fixture: ComponentFixture<ComicComponent>;
 
+  const mockData = {
+    num: 2176,
+    title: "title",
+    img: "https://imgs.xkcd.com/comics/how_hacking_works.png",
+    alt: "If only somebody had warned them that the world would roll them like this."
+  };
+
+  const xkcdServiceStub = jasmine.createSpyObj('XkcdService', ['init', 'getComic']);
+  xkcdServiceStub.init.and.returnValue(Promise.resolve());
+  xkcdServiceStub.getComic.and.returnValue(of(mockData));
+
   beforeEach(() => {
-    xkcdServiceStub = {
-      init: () => {return Promise.resolve()},
-      getComic: (id?:number) => { return new Observable(); }
-    }
 
     TestBed.configureTestingModule({
       imports: [RouterTestingModule.withRoutes([])],
       declarations: [ ComicComponent ],
       providers: [ 
         {provide: XkcdService, useValue: xkcdServiceStub},
-        {provide: ActivatedRoute, useValue: { snapshot: {paramMap: convertToParamMap({id: 1})}}}
+        {provide: ActivatedRoute, useValue: { paramMap: of(convertToParamMap({id: 1}))}}
       ]
     });
 
@@ -34,13 +42,41 @@ describe('ComicComponent', () => {
     component = fixture.componentInstance;
     xkcdService = TestBed.get(XkcdService);
     fixture.detectChanges();
+
   });
 
   afterEach(() => {
     fixture.destroy();
   });
 
-  it('should create', () => {
+  it('should show loading when created', () => {
     expect(component).toBeTruthy();
+    const headers = fixture.debugElement.queryAll(By.css('h1'));
+    expect(headers.length).toEqual(1);
+    expect(headers[0].nativeElement.innerText).toContain('Loading');
+    expect(xkcdService.init).toHaveBeenCalled();
   });
+
+  it('should load the mock data', async(() => {
+    fixture.detectChanges();
+    // wait for getComic observable to complete, then
+    fixture.whenStable().then(() => {
+      expect(xkcdService.getComic).toHaveBeenCalledWith(1); // route param set in activatedRoute
+      expect(component.comic.num).toEqual(mockData.num);
+    });
+  }));
+
+  it('should render the mock data', async(() => {
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      // data updated in component, now render
+      fixture.detectChanges();
+      fixture.whenRenderingDone().then(() => {
+        const headers = fixture.debugElement.queryAll(By.css('h1'));
+        expect(headers.length).toEqual(1);
+        expect(headers[0].nativeElement.innerText).toContain(mockData.title);
+        expect(headers[0].nativeElement.innerText).toContain(mockData.num);
+      });
+    });
+  }));
 });
